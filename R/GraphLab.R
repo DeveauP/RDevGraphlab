0080154c414345e88c2a943d2b1fc01f0412befc
DeveauP <paul.deveau@polytechnique.edu>
2016-03-18 11:25:36 +0100

    Functional interaction matrix generation

--- R/GraphLab.R
+++ R/GraphLab.R
@@ -7,25 +7,38 @@ GraphLab<-function(path = ""){
   tmp_env<-new.env()
   
   for(file in file.list){
-    source(file = file)
+    source(file = paste(path,file,sep ="/"),local = tmp_env,print.eval = TRUE)
   }
-  
-  env_items<-ls(envir = tmp_env) ### Does it take into account data too? (example : maps in ACSNMineR package)
-  functions<-env_items[apply(X = env_items,
-                   FUN = function(z) eval(parse(text = z))=="function"
-              )
-              ]
+
+  env_items<-unlist(ls(envir = tmp_env)) ### Does it take into account data too? (example : maps in ACSNMineR package)
+  functions<-env_items[sapply(X = env_items,
+                             FUN = function(z){
+                               e<-is.function(eval(parse(text = z)))
+                             } 
+                        )
+  ]
   
   ### Create interation matrix
   interaction_matrix<-matrix(0,
                              nrow = length(functions),
                              ncol = length(functions))
-  for(func in functions){
-      ### Is another function from  the function list called?
-      ### Are you importing from another package? (then which? => devtool::check)
-      
-      
-    }
+  for(i in 1:length(functions)){
+    ### Is another function from  the function list called?
+    ### Are you importing from another package? (then which? => devtool::check)
+    ### Storing vertically : 1 in i column j row means function i calls function j
+    interaction_matrix[,i]<-sapply(X = functions, FUN = function(z){  
+      ### body returns a vector => see if you call it more than once
+      ### We want function to start with the name and finish with parenthesis => exlude function that could
+      ### have partially identical names or items with same names as functions
+
+      x<-unlist(strsplit(x = as.character(body(functions[i])), split =" "))
+      a<-sum(grepl(pattern = paste("(\\(?)",z,'(\\()(.*)(\\))',sep=""),
+               x =  x)
+      )
+      print(a)
+      return(as.numeric(a>0))
+    })
+  }
   ### Get result from roxygen2 : missing/incomplete descriptions, exported or not, etc.
   ### If from roxygen: check looks for Rd files, may not be of interest for scripts
   
@@ -34,5 +47,11 @@ GraphLab<-function(path = ""){
 PlotGraphLab<-function(GraphLab){
   ### get interaction matrix and status for each function
   
+  
+}
+
+DevGraphLab<-function(path){
+  Graph<-GraphLab(path = path)
+  return(PlotGraphLab(GraphLab))
   
 }
